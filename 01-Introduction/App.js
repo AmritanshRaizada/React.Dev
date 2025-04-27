07bb21386e307ccfcf7dd439832b950ba9043d2f
AmritanshRaizada <ammritanshspc@gmail.com>
2025-04-27 13:28:50 +0530

    Refactor App.js to improve structure by nesting child elements

--- 01-Introduction/App.js
+++ 01-Introduction/App.js
@@ -1,7 +1,14 @@
 const parent = React.createElement('div', {id:'parent'}, 
-    React.createElement('div', {id:'child'}, 
-        React.createElement('h1', {id:'heading'}, 'Hello World')));
-const heading = React.createElement('h1', {id:'heading'}, 'Hello World');
+    [React.createElement('div', {id:'child'}, 
+        [React.createElement('h1', {id:'heading'}, 'Hello World'),
+         React.createElement('h2', {id:'heading'}, 'Hello World'),
+        ]),
+     React.createElement('div', {id:'child'}, 
+        [React.createElement('h1', {id:'heading'}, 'Hello World'),
+         React.createElement('h2', {id:'heading'}, 'Hello World'),
+         ])
+        ]);
+
 console.log(parent);
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(parent); 
